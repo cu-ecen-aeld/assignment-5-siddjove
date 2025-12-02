06737efe1fffb1fde71d5b62ab84b56232c468af
Sidd Jove <siddjove@gmail.com>
2025-12-02 20:29:04 +0530

    Add AESD external tree and aesd-assignments package

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,2 +1,2 @@
-# Path to this external tree
-BR2_EXTERNAL_PROJECT_BASE_PATH := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+# Extra make logic for the AESD external tree (currently none needed)
+
