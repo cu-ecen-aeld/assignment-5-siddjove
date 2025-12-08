cb9c1912c77776bb49d3c0b0203f77d2b8c901f2
Sidd Jove <siddjove@gmail.com>
2025-12-08 17:53:47 +0530

    Finalize Buildroot setup and aesd-assignments package

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,3 +1,7 @@
-# Tell Buildroot we have an external tree
-BR2_EXTERNAL_PROJECT_BASE_PATH := $(BR2_EXTERNAL)
+# External tree for AESD project base
 
+# Buildroot (when using external.desc) defines:
+#   BR2_EXTERNAL_PROJECT_BASE_PATH
+# We just need to include all external package .mk files.
+
+include $(sort $(wildcard $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/*/*.mk))
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,22 +4,22 @@
 #
 ################################################################################
 
-AESD_ASSIGNMENTS_VERSION = master
+# Use your A3+ repo as the source
+AESD_ASSIGNMENTS_VERSION = 89167357d86f8c4c8229b7e4bd986564285f01a8
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-siddjove.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
-AESD_ASSIGNMENTS_DEPENDENCIES =
-
+# Build the finder-app using the cross compiler
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) -C $(@D)/finder-app CC="$(TARGET_CC)"
 endef
 
+# Install scripts and binaries into target rootfs
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	# Scripts and binaries into /usr/bin
 	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
 	$(INSTALL) -D -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
 	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
-	$(INSTALL) -D -m 0755 $(@D)/finder-app/tester.sh $(TARGET_DIR)/usr/bin/tester.sh || true
 
 	# Config files into /etc/finder-app/conf
 	$(INSTALL) -d $(TARGET_DIR)/etc/finder-app/conf
