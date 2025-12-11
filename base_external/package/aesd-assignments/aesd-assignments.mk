592c3a4bd272cf9443815a5a1dc3b85d2119814d
Sidd Jove <siddjove@gmail.com>
2025-12-11 20:26:35 +0530

    Fix: set AESD_ASSIGNMENTS_VERSION to 61d334296210877130c40c5a6d4153f1c27d1e58; add submodule URL for buildroot-assignments-base

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '36b939084c4e2393df919e3bd2cc687379a9895c'
+AESD_ASSIGNMENTS_VERSION = '61d334296210877130c40c5a6d4153f1c27d1e58'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-siddjove.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-siddjove.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -40,3 +40,6 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 
 endef
+
+AESD_ASSIGNMENTS_SUBMODULES = YES
+AESD_ASSIGNMENTS_SUBMODULES_URL = git@github.com:cu-ecen-aeld/buildroot-assignments-base.git
