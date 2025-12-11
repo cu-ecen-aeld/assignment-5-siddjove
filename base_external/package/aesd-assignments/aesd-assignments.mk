473e4fc85d14d487d991cc96f555afd85e64db37
Sidd Jove <siddjove@gmail.com>
2025-12-11 20:29:58 +0530

    Fix: set AESD_ASSIGNMENTS_VERSION to 61d334296210877130c40c5a6d4153f1c27d1e58; add AESD_ASSIGNMENTS_SUBMODULES URL

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '61d334296210877130c40c5a6d4153f1c27d1e58'
+AESD_ASSIGNMENTS_VERSION = 61d334296210877130c40c5a6d4153f1c27d1e58
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
