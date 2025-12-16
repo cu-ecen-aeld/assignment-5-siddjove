e575c06673282b327e9871d42276a9f8c9f8e2c3
Sidd Jove <siddjove@gmail.com>
2025-12-16 12:44:04 +0530

    Fix aesd_assignments download to use git repository

--- base_external/package/aesd_assignments/aesd_assignments.mk
+++ base_external/package/aesd_assignments/aesd_assignments.mk
@@ -1,20 +1,17 @@
+AESD_ASSIGNMENTS_VERSION = master
 AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-siddjove.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_VERSION = master
+
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket \
 		$(TARGET_DIR)/usr/bin/aesdsocket
-
-	$(INSTALL) -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop \
 		$(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
-
