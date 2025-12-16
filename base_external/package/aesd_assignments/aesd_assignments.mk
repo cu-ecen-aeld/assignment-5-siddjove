204cd660a6bba55405e2af121f841ee4d290a612
Sidd Jove <siddjove@gmail.com>
2025-12-16 14:38:06 +0530

    Update aesd-assignments to latest commit cb13be7

--- base_external/package/aesd_assignments/aesd_assignments.mk
+++ base_external/package/aesd_assignments/aesd_assignments.mk
@@ -1,17 +1,37 @@
-AESD_ASSIGNMENTS_VERSION = master
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+AESD_ASSIGNMENTS_VERSION = cb13be7
 AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-siddjove.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
-
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket \
-		$(TARGET_DIR)/usr/bin/aesdsocket
+	# conf
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf
+	$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+
+	# autotest
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
+	# finder utilities
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+
+	# aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop \
 		$(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
+
