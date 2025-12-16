6b2c2d11b47236e5e243487b88f2b2032d8c3a9c
Sidd Jove <siddjove@gmail.com>
2025-12-16 15:53:29 +0530

    Fix package name to aesd-assignments for autograder compatibility

---
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -0,0 +1,24 @@
+##############################################################
+#
+# AESD_ASSIGNMENTS
+#
+##############################################################
+
+AESD_ASSIGNMENTS_VERSION = 1d5c252
+AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-siddjove.git
+AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE) -C $(@D)/server clean
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
+endef
+
+define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop \
+		$(TARGET_DIR)/etc/init.d/S99aesdsocket
+endef
+
+$(eval $(generic-package))
+
