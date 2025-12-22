c5a866f63f32475fe3bfce079a0f750619dd3d23
Sidd Jove <siddjove@gmail.com>
2025-12-22 16:59:24 +0530

    Update aesd-assignments commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 8a4b007 
+AESD_ASSIGNMENTS_VERSION = ff3044c 
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-siddjove.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
