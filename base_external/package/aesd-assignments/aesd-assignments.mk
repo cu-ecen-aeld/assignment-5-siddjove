259486785851c933c4e8b9b1bd79c5f288e0110a
Sidd Jove <siddjove@gmail.com>
2025-12-22 18:25:59 +0530

    Update aesd-assignments to clean blocking implementation

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,21 +4,27 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 8265e30
+AESD_ASSIGNMENTS_VERSION = 2eff8a8 
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-siddjove.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) -C $(@D)/server clean
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
+	$(MAKE) CROSS_COMPILE=$(TARGET_CROSS) -C $(@D)/finder-app all
+	$(MAKE) CROSS_COMPILE=$(TARGET_CROSS) -C $(@D)/server all
 endef
 
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop \
-		$(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 endef
 
 $(eval $(generic-package))
-
